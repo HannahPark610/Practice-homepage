fd2eb3e5d5f84998fa272b39b9ff329e62dc5194
Hannah Park <teyang610@gmail.com>
2017-01-09 13:34:04 -0800

    modify

--- js/instagram.js
+++ js/instagram.js
@@ -1,7 +1,7 @@
-$.ajax(
-
-
-  {
+$(function() {
+  console.log('ready');
+});
+$.ajax({
   url: "https://api.instagram.com/v1/users/self/?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
   dataType:'jsonp',
   success: function(response){
@@ -9,10 +9,10 @@ $.ajax(
     console.log(response.data.username);
   },
 });
-} else {
-     console.log("I want login");
-     $("#login").show();
-     $("#info").hide();
-     $("#logout").hide();
-   }
- });
+// } else {
+//      console.log('success');
+//      $("#login").show();
+//      $("#info").hide();
+//      $("#logout").hide();
+//    }
+//  });

74b4c0d213759fb137e8781ff500df1c944d50db
Hannah Park <teyang610@gmail.com>
2017-01-09 12:45:51 -0800

    modify

--- js/instagram.js
+++ js/instagram.js
@@ -1,9 +1,18 @@
-$.ajax({
+$.ajax(
+
+
+  {
   url: "https://api.instagram.com/v1/users/self/?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
   dataType:'jsonp',
-}).done(function (data) {
-  console.log(data);
-  document.getElmentById("hannahuser").innerHTML = data.data.username;
-}).error(funtion(err){
-
+  success: function(response){
+    console.log('success');
+    console.log(response.data.username);
+  },
 });
+} else {
+     console.log("I want login");
+     $("#loginArea").show();
+     $("#infoArea").hide();
+     $("#logoutArea").hide();
+   }
+ });

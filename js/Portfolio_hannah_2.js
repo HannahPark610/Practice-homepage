317323e36e72a94dccb37241632e5608d9781dab
Hannah Park <teyang610@gmail.com>
2017-01-11 11:56:28 -0800

    portfolio_ updated

--- js/Portfolio_hannah_2.js
+++ js/Portfolio_hannah_2.js
@@ -1,13 +1,29 @@
 $(function() {
   console.log('ready');
-});
-$.ajax({
-  url: "https://api.instagram.com/v1/users/self/?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
-  dataType:'jsonp',
-  success: function(response){
-    console.log('success');
-    console.log(response.data.username);
-  },
+
+  $("#showFollowers").click(function () {
+    $.ajax({
+      url: "https://api.instagram.com/v1/users/{user-id}/relationship?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
+      dataType:'jsonp',
+      success: function(response){
+        console.log('success');
+        console.log(response);
+        for(let relationship of response.data) {
+          console.log(relationship.full_name);
+          $("#relationships").append("<img src=" + relationship.profile_picture + "><p>"+relationship.full_name+"</p>");
+        }
+      },
+    });
+  });
+
+  $.ajax({
+    url: "https://api.instagram.com/v1/users/self/?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
+    dataType:'jsonp',
+    success: function(response){
+      console.log('success');
+      console.log(response.data.username);
+    },
+  });
 });
 
 $(function(){

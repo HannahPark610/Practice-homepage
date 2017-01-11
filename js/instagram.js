400d3a5f0433a69ba9cf6403cfdc99a628d39fc0
Hannah Park <teyang610@gmail.com>
2017-01-11 11:26:39 -0800

    modify

--- js/instagram.js
+++ js/instagram.js
@@ -1,28 +1,32 @@
 $(function() {
   console.log('ready');
-});
-$.ajax({
-  url: "https://api.instagram.com/v1/users/self/?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
-  dataType:'jsonp',
-  success: function(response){
-    console.log('success');
-    console.log(response.data.username);
-  },
-});
-$("#showFollowers").click(function () {
+
+  $("#showFollowers").click(function () {
+    $.ajax({
+      url: "https://api.instagram.com/v1/users/self/follows?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
+      dataType:'jsonp',
+      success: function(response){
+        //console.log('success');
+        //console.log(response.data.username);
+        for(let follower of response.data) {
+          console.log(follower.full_name);
+          $("#followers").append("<img src=" + follower.profile_picture + "><p>"+follower.full_name+"</p>");
+        }
+      },
+    });
+  });
+
   $.ajax({
-    url: "https://api.instagram.com/v1/users/self/follows?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
+    url: "https://api.instagram.com/v1/users/self/?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
     dataType:'jsonp',
     success: function(response){
-      //console.log('success');
-      //console.log(response.data.username);
-      for(let follower of response.data) {
-        console.log(follower.full_name);
-        $("#followers").append("<img src=" + follower.profile_picture + "><p>"+follower.full_name+"</p>");
-      }
+      console.log('success');
+      console.log(response.data.username);
     },
   });
 });
+
+
 
 
 // } else {

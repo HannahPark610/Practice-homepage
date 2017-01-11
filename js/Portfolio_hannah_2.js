a2f1f72e3fcbb2e4a8b2cf5c244901364ef4ba2c
Hannah Park <teyang610@gmail.com>
2017-01-11 10:52:48 -0800

    instagram

--- js/Portfolio_hannah_2.js
+++ js/Portfolio_hannah_2.js
@@ -1,3 +1,15 @@
+$(function() {
+  console.log('ready');
+});
+$.ajax({
+  url: "https://api.instagram.com/v1/users/self/?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
+  dataType:'jsonp',
+  success: function(response){
+    console.log('success');
+    console.log(response.data.username);
+  },
+});
+
 $(function(){
 
   $("#bar1").click(function(){
--- js/instagram.js
+++ js/instagram.js
@@ -9,6 +9,20 @@ $.ajax({
     console.log(response.data.username);
   },
 });
+
+$.ajax({
+  url: "https://api.instagram.com/v1/users/self/follows?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
+  dataType:'jsonp',
+  success: function(response){
+    //console.log('success');
+    //console.log(response.data.username);
+    for(let follower of response.data) {
+      $("#followers").append("<img src=" + follower.profile_picture + "><p>"+follower.full_name+"</p>");
+    }
+  },
+});
+
+
 // } else {
 //      console.log('success');
 //      $("#login").show();

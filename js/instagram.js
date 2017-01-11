c36af84c4831acd8e069af8b7f5a8c061e68e21a
Hannah Park <teyang610@gmail.com>
2017-01-11 11:32:35 -0800

    modify

--- js/instagram.js
+++ js/instagram.js
@@ -3,11 +3,11 @@ $(function() {
 
   $("#showFollowers").click(function () {
     $.ajax({
-      url: "https://api.instagram.com/v1/users/self/follows?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
+      url: "https://api.instagram.com/v1/users/self/followed-by?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
       dataType:'jsonp',
       success: function(response){
-        //console.log('success');
-        //console.log(response.data.username);
+        console.log('success');
+        console.log(response);
         for(let follower of response.data) {
           console.log(follower.full_name);
           $("#followers").append("<img src=" + follower.profile_picture + "><p>"+follower.full_name+"</p>");

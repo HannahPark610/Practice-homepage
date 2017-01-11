351bffb5e821edad4bb09fa6f0eaa9d778ca81f7
Hannah Park <teyang610@gmail.com>
2017-01-11 11:09:06 -0800

    added a button for followers

--- js/instagram.js
+++ js/instagram.js
@@ -9,18 +9,19 @@ $.ajax({
     console.log(response.data.username);
   },
 });
-
-$.ajax({
-  url: "https://api.instagram.com/v1/users/self/follows?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
-  dataType:'jsonp',
-  success: function(response){
-    //console.log('success');
-    //console.log(response.data.username);
-    for(let follower of response.data) {
-      console.log(follower.full_name);
-      $("#followers").append("<img src=" + follower.profile_picture + "><p>"+follower.full_name+"</p>");
-    }
-  },
+$("#showFollowers").click(function () {
+  $.ajax({
+    url: "https://api.instagram.com/v1/users/self/follows?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
+    dataType:'jsonp',
+    success: function(response){
+      //console.log('success');
+      //console.log(response.data.username);
+      for(let follower of response.data) {
+        console.log(follower.full_name);
+        $("#followers").append("<img src=" + follower.profile_picture + "><p>"+follower.full_name+"</p>");
+      }
+    },
+  });
 });
 
 

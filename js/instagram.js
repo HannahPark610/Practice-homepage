fb5d3987eb9940f01a6d11d29370b0881c6895ca
Hannah Park <teyang610@gmail.com>
2017-01-12 12:09:51 -0800

    updated_instagram_portfolio

--- js/instagram.js
+++ js/instagram.js
@@ -28,11 +28,42 @@ $(function() {
 
 
 
-
-// } else {
-//      console.log('success');
-//      $("#login").show();
-//      $("#info").hide();
-//      $("#logout").hide();
-//    }
-//  });
+$(function() {
+     var tocken = "1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea"; /* Access Tocken 입력 */
+     var count = "6";
+     $.ajax({
+         type: "GET",
+         dataType: "jsonp",
+         cache: false,
+         url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea&count=6",
+         success: function(response) {
+          if ( response.data.length > 0 ) {
+               for(var i = 0; i < response.data.length; i++) {
+                    var insta = '<div class="insta-box">';
+                    insta += "<a target='_blank' href='" + response.data[i].link + "'>";
+                    insta += "<div class'image-layer'>";
+                    //insta += "<img src='" + response.data[i].images.thumbnail.url + "'>";
+                    insta += '<img src="' + response.data[i].images.thumbnail.url + '">';
+                    insta += "</div>";
+                    //console.log(response.data[i].caption.text);
+                    if ( response.data[i].caption !== null ) {
+                         insta += "<div class='caption-layer'>";
+                         if ( response.data[i].caption.text.length > 0 ) {
+                              insta += "<p class='insta-caption'>" + response.data[i].caption.text + "</p>"
+                         }
+                         insta += "<span class='insta-likes'>" + response.data[i].likes.count + " Likes</span>";
+                         insta += "</div>";
+                    }
+                    insta += "</a>";
+                    insta += "</div>";
+                    $("#instaPics").append(insta);
+               }
+          }
+          $(".insta-box").hover(function(){
+               $(this).find(".caption-layer").css({"backbround" : "rgba(255,255,255,0.7)", "display":"block"});
+          }, function(){
+               $(this).find(".caption-layer").css({"display":"none"});
+          });
+         }
+        });
+});

2011d1c37428d785b9597755b485875be44e22bf
Hannah Park <teyang610@gmail.com>
2017-01-15 21:28:21 -0800

    move to main folder

--- js/insta.js
+++ js/insta.js
@@ -55,7 +55,7 @@ $(function() {
                               for(var i = 0; i < response.data.length; i++) {
                                 var html = '<ul>';
                                 $.each(response.data, function() {
-                                html += '<li><img src="' + this.link + '" alt="' + this.title + '"></li>';
+                                html += '<li><img src="' + this.images.standard_resolution.url + '" alt="' + this.title + '"></li>';
                             });
 
                             html += '</ul>';
@@ -69,6 +69,21 @@ $(function() {
                         };
                       }
                     }});
+
+                      $("#followers").click(function() {
+                        $.ajax({
+                          url: "https://api.instagram.com/v1/users/self/followed-by?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
+                          dataType:'jsonp',
+                          success: function(response){
+                            console.log('success');
+                            console.log(response);
+                            for(let follower of response.data) {
+                              console.log(follower.full_name);
+                              $("#login").append("<img src=" + follower.profile_picture + "><p>"+follower.full_name+"</p>");
+                            }
+                          },
+                        });
+                      });
 });
 
 function logoutInsta() {
@@ -80,6 +95,7 @@ function showLogin() {
   $("#logout").hide();
   $("#my_logo").show();
   $("#thumbnail").hide();
+  $("#followers").hide();
 }
 
 function hideLogin() {
@@ -87,4 +103,5 @@ function hideLogin() {
   $("#logout").show();
   $("#my_logo").hide();
   $("#thumbnail").show();
+  $("#followers").show();
 }

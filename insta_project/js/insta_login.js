11ef27d263af5b1b871d00e3fad2771587d7a024
Hannah Park <teyang610@gmail.com>
2017-01-15 21:01:27 -0800

    update

--- insta_project/js/insta_login.js
+++ insta_project/js/insta_login.js
@@ -4,19 +4,71 @@ $(function() {
   if (urlanchor !== "") {
     localStorage.setItem("insta-token", token[1]);
       hideLogin();
-      $("#my_logo").hide();
-      $("#thumbnail").show();
   }
 
   if (localStorage.getItem("insta-token") == null) {
     showLogin();
-    $("#my_logo").show();
-    $("#thumbnail").hide();
   } else {
     hideLogin();
-    $("#my_logo").hide();
-    $("#thumbnail").show();
   }
+
+  var jcarousel = $('.jcarousel').jcarousel();
+
+          $('.jcarousel-control-prev')
+              .on('jcarouselcontrol:active', function() {
+                  $(this).removeClass('inactive');
+              })
+              .on('jcarouselcontrol:inactive', function() {
+                  $(this).addClass('inactive');
+              })
+              .jcarouselControl({
+                  target: '-=1'
+              });
+
+          $('.jcarousel-control-next')
+              .on('jcarouselcontrol:active', function() {
+                  $(this).removeClass('inactive');
+              })
+              .on('jcarouselcontrol:inactive', function() {
+                  $(this).addClass('inactive');
+              })
+              .jcarouselControl({
+                  target: '+=1'
+              });
+
+          $('.jcarousel-pagination')
+              .on('jcarouselpagination:active', 'a', function() {
+                  $(this).addClass('active');
+              })
+              .on('jcarouselpagination:inactive', 'a', function() {
+                  $(this).removeClass('active');
+              })
+              .jcarouselPagination();
+
+                  $.ajax({
+                      type: "GET",
+                      dataType: "jsonp",
+                      cache: false,
+                      url: "https://api.instagram.com/v1/users/self/media/recent/?access_token=" + localStorage.getItem("insta-token"),
+                      success: function(response) {
+                         if ( response.data.length > 0 ) {
+                              for(var i = 0; i < response.data.length; i++) {
+                                var html = '<ul>';
+                                $.each(response.data, function() {
+                                html += '<li><img src="' + this.link + '" alt="' + this.title + '"></li>';
+                            });
+
+                            html += '</ul>';
+                            // Append items
+                            jcarousel
+                                .html(html);
+
+                            // Reload carousel
+                            jcarousel
+                                .jcarousel('reload');
+                        };
+                      }
+                    }});
 });
 
 function logoutInsta() {
@@ -26,45 +78,13 @@ function logoutInsta() {
 function showLogin() {
   $("#login").show();
   $("#logout").hide();
+  $("#my_logo").show();
+  $("#thumbnail").hide();
 }
 
 function hideLogin() {
   $("#login").hide();
   $("#logout").show();
+  $("#my_logo").hide();
+  $("#thumbnail").show();
 }
-
-$(".jcarousel").click(function(login) {
-        $('.jcarousel').jcarousel();
-
-        $('.jcarousel-control-prev')
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .jcarouselControl({
-                target: '-=1'
-            });
-
-        $('.jcarousel-control-next')
-            .on('jcarouselcontrol:active', function() {
-                $(this).removeClass('inactive');
-            })
-            .on('jcarouselcontrol:inactive', function() {
-                $(this).addClass('inactive');
-            })
-            .jcarouselControl({
-                target: '+=1'
-            });
-
-        $('.jcarousel-pagination')
-            .on('jcarouselpagination:active', 'a', function() {
-                $(this).addClass('active');
-            })
-            .on('jcarouselpagination:inactive', 'a', function() {
-                $(this).removeClass('active');
-            })
-            .jcarouselPagination();
-    });
-})(jQuery);

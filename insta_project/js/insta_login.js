144f27bc98746e87854a2e8732a36787213efb73
Hannah Park <teyang610@gmail.com>
2017-01-15 18:12:59 -0800

    updated

--- insta_project/js/insta_login.js
+++ insta_project/js/insta_login.js
@@ -1,11 +1,2 @@
 $(function() {
-  $("#login").click(function() {
-    $.ajax({
-        url: 'https://api.instagram.com/oauth/authorize/?client_id=6eaddd3646c44539a3a10b0f517076ca&redirect_uri=https://hannahpark610.github.io/insta_project&response_type=token',
-        dataType:'jsonp',
-        success: function(response){
-          alert(respose);
-        }
-      });
-  });
 });

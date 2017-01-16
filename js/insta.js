fdff91c684b508469d58562266914830c5f30fc7
Hannah Park <teyang610@gmail.com>
2017-01-15 22:34:48 -0800

    update

--- js/insta.js
+++ js/insta.js
@@ -84,7 +84,7 @@ element.jcarousel('items').css('width', width + 'px');
 
                       $("#followers").click(function() {
                         $.ajax({
-                          url: "https://api.instagram.com/v1/users/self/followed-by?access_token=1428507007.6eaddd3.53bb720480a8444cb595086c5a494eea",
+                          url: "https://api.instagram.com/v1/users/self/followed-by?access_token=" + localStorage.getItem("insta-token"),
                           dataType:'jsonp',
                           success: function(response){
                             console.log('success');

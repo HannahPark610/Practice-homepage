abea4f87a5bd119999638db97e23aef89b20c30d
Hannah Park <teyang610@gmail.com>
2017-01-15 19:39:12 -0800

    update

--- insta_project/js/insta_login.js
+++ insta_project/js/insta_login.js
@@ -2,13 +2,20 @@ $(function() {
   var urlanchor = location.hash;
   var token = urlanchor.split("=");
   if (urlanchor !== "") {
-    localStorage.setItem("insta-token", token[1])
+    localStorage.setItem("insta-token", token[1]);
+      hideLogin();
+      $("#my_logo").hide();
+      $("#thumbnail").show();
   }
 
   if (localStorage.getItem("insta-token") == null) {
     showLogin();
+    $("#my_logo").show();
+    $("#thumbnail").hide();
   } else {
     hideLogin();
+    $("#my_logo").hide();
+    $("#thumbnail").show();
   }
 });
 
@@ -25,3 +32,39 @@ function hideLogin() {
   $("#login").hide();
   $("#logout").show();
 }
+
+$(".jcarousel").click(function(login) {
+        $('.jcarousel').jcarousel();
+
+        $('.jcarousel-control-prev')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '-=1'
+            });
+
+        $('.jcarousel-control-next')
+            .on('jcarouselcontrol:active', function() {
+                $(this).removeClass('inactive');
+            })
+            .on('jcarouselcontrol:inactive', function() {
+                $(this).addClass('inactive');
+            })
+            .jcarouselControl({
+                target: '+=1'
+            });
+
+        $('.jcarousel-pagination')
+            .on('jcarouselpagination:active', 'a', function() {
+                $(this).addClass('active');
+            })
+            .on('jcarouselpagination:inactive', 'a', function() {
+                $(this).removeClass('active');
+            })
+            .jcarouselPagination();
+    });
+})(jQuery);
